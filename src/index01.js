b1b7a64d1a8eaea6a9c257017aa642976e9ed8d1
Luanna Fernandes Flor de Souza <141292139+Luanna88@users.noreply.github.com>
2023-10-18 13:58:07 -0300

    Update index01.js

--- src/index01.js
+++ src/index01.js
@@ -1,3 +1,3 @@
-var qrcode = require('qrcode-terminal');
+var qrcode = require('qrcode-terminal'); // Faça a importação de npm i react-qr-code; e=use o site https://www.npmjs.com/package/react-qr-code
 
 qrcode.generate('This will be a QRCode, eh!');
